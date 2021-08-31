9bccd1e2e28050378fd2d959da5936d75e3de139
barreirabruno <brunobarreirasilva@gmail.com>
2021-08-31 17:04:52 -0300

    feat: add Category class

---
+++ entity/src/main/java/br/com/expensecontrolapplication/entity/Category.java
@@ -0,0 +1,25 @@
+package br.com.expensecontrolapplication.entity;
+
+import java.util.Arrays;
+
+public enum Category {
+
+    FOOD("Food"),
+    MOBILITY("Mobility"),
+    HOBBY("Hobby"),
+    OTHER("Other");
+
+    private String category;
+
+    Category(String category) {
+        this.category = category;
+    }
+
+    public static Category of(String value) {
+        return Arrays.stream(values()).filter(type -> type.category.equalsIgnoreCase(value)).findFirst().orElse(null);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
