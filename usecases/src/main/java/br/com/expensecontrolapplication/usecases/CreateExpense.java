3bfc9a70b92cd91d6fb6d330453068e02521666f
barreirabruno <brunobarreirasilva@gmail.com>
2021-08-31 22:52:55 -0300

    feat: add CreateExpense usecase

---
+++ usecases/src/main/java/br/com/expensecontrolapplication/usecases/CreateExpense.java
@@ -0,0 +1,19 @@
+package br.com.expensecontrolapplication.usecases;
+
+import br.com.expensecontrolapplication.entity.Expense;
+import br.com.expensecontrolapplication.usecases.port.ExpenseRepository;
+
+public class CreateExpense {
+
+    private final ExpenseRepository repository;
+
+    public CreateExpense(ExpenseRepository repository) {
+        this.repository = repository;
+    }
+
+    public Expense create(final Expense expense) {
+        System.out.println("[EXPENSE REPOSITORY]" + expense);
+        var createdExpense = repository.create(expense);
+        return createdExpense;
+    }
+}
